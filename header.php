f6074cea8d624200e93806016f0da8e133fd824f
Carolina Hagman <hagmancarolina@gmail.com>
2020-10-20 13:26:34 +0200

    updates

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/global.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/article.css">
+    <title>Fake News</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="head-title">FAKE NEWS</h1>
+    </header>
--- index.php
+++ index.php
@@ -2,26 +2,13 @@
 
 require __DIR__ . '/data.php';
 require __DIR__ . '/functions.php';
-
+require __DIR__ . '/header.php';
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap" rel="stylesheet">
-  <title>Fake News</title>
-</head>
-
-<body>
-  <header>
-    <h1 class="head-title">FAKE NEWS</h1>
-  </header>
-  <main>
-    <article class="article-preview"></article>
-  </main>
+<main>
+  <article class="article-preview"></article>
+</main>
 </body>
 
 </html>
