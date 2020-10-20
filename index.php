fbb02bc57acddfc7469b86d8cb9c2ab8fa3f958e
Carolina Hagman <hagmancarolina@gmail.com>
2020-10-20 13:15:59 +0200

    added images and database

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php
 
-// This is the file where you can keep your HTML markup. We should always try to
-// keep us much logic out of the HTML as possible. Put the PHP logic in the top
-// of the files containing HTML or even better; in another PHP file altogether.
+require __DIR__ . '/data.php';
+require __DIR__ . '/functions.php';
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap" rel="stylesheet">
+  <title>Fake News</title>
 </head>
 
 <body>
+  <header>
+    <h1 class="head-title">FAKE NEWS</h1>
+  </header>
+  <main>
+    <article class="article-preview"></article>
+  </main>
 </body>
 
 </html>
