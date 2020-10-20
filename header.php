c0e7f45bf2afe47597bdcc82729a89fb2fa620f4
Carolina Hagman <hagmancarolina@gmail.com>
2020-10-20 13:51:34 +0200

    added basic css

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Neuton&family=Playfair+Display&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style/global.css">
     <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="style/article.css">
@@ -13,5 +13,5 @@
 
 <body>
     <header>
-        <h1 class="head-title">FAKE NEWS</h1>
+        <h1 class="head-title">Fake News</h1>
     </header>
