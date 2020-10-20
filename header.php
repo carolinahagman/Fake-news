624252ad81333fdf93b5bd516e96602aaeb871ee
Carolina Hagman <hagmancarolina@gmail.com>
2020-10-20 16:51:07 +0200

    added content

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Neuton&family=Playfair+Display&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style/global.css">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/article-preview.css">
     <link rel="stylesheet" href="style/article.css">
     <title>Fake News</title>
 </head>
@@ -14,5 +15,5 @@
 <body>
     <header>
         <h1 class="head-title">Fake News</h1>
-        <hr>
+        <hr class="line-header">
     </header>
--- index.php
+++ index.php
@@ -7,7 +7,18 @@ require __DIR__ . '/header.php';
 ?>
 
 <main>
-  <article class="article-preview"></article>
+  <section class="article-preview">
+    <h2 class="title"></h2>
+    <div class="article-info">
+      <button class="like-btn"></button>
+      <p class="date"></p>
+      <p class="author"></p>
+      <button class="read-more-btn"><a href="">Read More</a>
+      </button>
+    </div>
+    <div class="img-preview"><img src="/img/" alt=""></div>
+  </section>
+  <hr class="separate-line">
 </main>
 </body>
 
