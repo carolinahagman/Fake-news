02aea48920261ef0b925e30ba0467ca8048abd72
Carolina Hagman <hagmancarolina@gmail.com>
2020-10-21 10:50:57 +0200

    styled article-preview

--- index.php
+++ index.php
@@ -5,24 +5,24 @@ require __DIR__ . '/functions.php';
 require __DIR__ . '/header.php';
 
 ?>
-
-<main>
-  <section class="article-preview">
-    <h2 class="title">Don’t be fooled by fake news</h2>
-    <div class="article-sub-header">
-      <div class="article-info">
-        <button class="like-btn"> <img src="/img/heart.svg" alt="heart">like</button>
-        <p class="date">date</p>
-        <p class="author">Carolina Hagman</p>
-        <button class="read-more-btn"><a href="">Read More</a>
-        </button>
+<?php foreach ($posts as $post) : ?>
+  <main>
+    <h2 class="title"><?= $post['title']; ?></h2>
+    <section class="article-preview">
+      <div class="article-sub-header">
+        <div class="article-info">
+          <button class="like-btn"> <img class="heart-like" src="/img/heart.svg" alt="heart"></button>
+          <p class="date"><?= $post['date']; ?></p>
+          <p class="author">Carolina</p>
+          <button class="read-more-btn"><a class="read-more" href="">Read More</a>
+          </button>
+        </div>
+        <div class="img-preview"><img class="img-preview-item" src="/img/<?= $post['image']; ?>" alt=""></div>
       </div>
-      <div class="img-preview"><img class="img-preview-item" src="/img/1.jpg" alt=""></div>
-    </div>
-    <hr class="divider">
-  </section>
+      <hr class="divider">
+    </section>
+  <?php endforeach; ?>
+  </main>
+  </body>
 
-</main>
-</body>
-
-</html>
+  </html>
